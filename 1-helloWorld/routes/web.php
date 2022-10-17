2fb2d8cd27c735de5d7b60f2646c1a8360301f9b
stitou-nada <nadastito1087@gmail.com>
2022-10-17 16:50:12 +0100

    defi1

--- 1-helloWorld/routes/web.php
+++ 1-helloWorld/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\testContoller;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/test',[testContoller::class,'test']);
+
+
+
+
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
