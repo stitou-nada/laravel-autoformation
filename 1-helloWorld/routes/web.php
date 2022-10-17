fa321caa9da3daf43f37430111eb0caadef7e090
stitou-nada <nadastito1087@gmail.com>
2022-10-17 13:57:39 +0100

    start kids

--- 1-helloWorld/routes/web.php
+++ 1-helloWorld/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\testContoller;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +13,12 @@ use App\Http\Controllers\testContoller;
 |
 */
 
-Route::get('/test', function () {
-    return view('test');
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::get('/test',[testContoller::class,"test"]);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
