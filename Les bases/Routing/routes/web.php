6afc682d970d615342c46d28e90dd14f45da3960
stitou-nada <nadastito1087@gmail.com>
2022-10-18 14:27:37 +0100

    routage

--- Les bases/Routing/routes/web.php
+++ Les bases/Routing/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/index', function () {
-    return ('hello world');
-});
+// Route::get('/index', function () {
+//     return ('hello world');
+// });
+
+
+        // Les fichiers de routage par défaut
+Route::get('/index',[UserController::class,'index']);
