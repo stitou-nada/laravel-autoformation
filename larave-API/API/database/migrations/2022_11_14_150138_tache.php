2e2282dde878e04d81bc8edaa96f3b62eabb87d5
stitou-nada <nadastito1087@gmail.com>
2022-11-14 17:02:46 +0100

    api

--- larave-API/API/database/migrations/2022_11_14_150138_tache.php
+++ larave-API/API/database/migrations/2022_11_14_150138_tache.php
@@ -13,16 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('tache', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string("Nom_de_la_tache")->nullable();
+
         });
     }
+    
 
     /**
      * Reverse the migrations.
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        //
     }
 };
