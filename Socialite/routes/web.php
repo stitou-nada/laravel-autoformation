1a355c909b634cf86d05ac799626764f798b866e
stitou-nada <nadastito1087@gmail.com>
2022-12-21 11:32:36 +0100

    autofomation de socialite

--- Socialite/routes/web.php
+++ Socialite/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\githubController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +32,32 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Route::get('/auth/redirect', function () {
+//     return Socialite::driver('github')->redirect();
+// });
+
+
+
+// Route::get('/auth/callback', function () {
+//     $githubUser = Socialite::driver('github')->user();
+
+//     $user = User::updateOrCreate([
+//         'github_id' => $githubUser->id,
+//     ], [
+//         'name' => $githubUser->name,
+//         'email' => $githubUser->email,
+//         'github_token' => $githubUser->token,
+//         'github_refresh_token' => $githubUser->refreshToken,
+//     ]);
+
+//     Auth::login($user);
+
+//     return redirect('/dashboard');
+// })->name('authCallback');;
+
+
+Route::get('auth/github',[githubController::class,'redirect'])->name('github-auth');
+Route::get('auth/github/call-back',[githubController::class,'callbackGithub'])->name("callbackGithub");
+
+
